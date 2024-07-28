6b78993c2abe317f35cfc31caf0d96f81aef63f4
Connor-Y-Sokolov <Connor.Y.Sokolov@outlook.com>
2024-07-28 19:18:50 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use rand::{Rng, thread_rng};
 use std::cmp::Ordering::{Equal, Less, Greater}; 
 use std::io::stdin; 
 
-// 将game函数重命名为play_guessing_game，并接受一个u32类型的参数
 fn play_guessing_game(secret_number: u32) {
     loop {
         println!("Please input your guess.");
@@ -36,6 +35,5 @@ fn main() {
 
     let secret_number = thread_rng().gen_range(1..=100); 
 
-    // 调用play_guessing_game函数，并将secret_number传递给它
     play_guessing_game(secret_number);
 }
